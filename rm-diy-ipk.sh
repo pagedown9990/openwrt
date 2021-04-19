cf415cb0ed2b9384d0d765eb03c5b685002f4f66
hibuddies <odpazi04973@gmail.com>
2021-04-19 11:34:25 +0800

    update

---
+++ rm-diy-ipk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf /home/zlq/bianyi/ledeipk/package/luci-app-openclash
+rm -rf /home/zlq/bianyi/ledeipk/package/luci-app-ssr-plus
+rm -rf /home/zlq/bianyi/ledeipk/package/luci-app-passwall
+rm -rf /home/zlq/bianyi/ledeipk/package/luci-app-vssr
+rm -rf /home/zlq/bianyi/ledeipk/package/luci-app-bypass
+
+rm -rf /home/zlq/bianyi/ledeipk/package/brook
+rm -rf /home/zlq/bianyi/ledeipk/package/chinadns-ng
+rm -rf /home/zlq/bianyi/ledeipk/package/tcping
+rm -rf /home/zlq/bianyi/ledeipk/package/trojan-go
+rm -rf /home/zlq/bianyi/ledeipk/package/trojan-plus
+rm -rf /home/zlq/bianyi/ledeipk/package/ssocks
+rm -rf /home/zlq/bianyi/ledeipk/package/xray-core
+rm -rf /home/zlq/bianyi/ledeipk/package/xray-plugin
+rm -rf /home/zlq/bianyi/ledeipk/package/naiveproxy
+rm -rf /home/zlq/bianyi/ledeipk/package/shadowsocks-rust
+rm -rf /home/zlq/bianyi/ledeipk/package/v2ray-plugin
+rm -rf /home/zlq/bianyi/ledeipk/package/lua-maxminddb
