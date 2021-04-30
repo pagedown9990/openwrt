b3585645996c10e1ed047e330144de48319fcb26
hibuddies <75357479+hibuddies@users.noreply.github.com>
2021-04-30 09:19:27 +0800

    改回用f大仓库打包

--- ARMv8/respawn_mks.sh
+++ ARMv8/respawn_mks.sh
@@ -11,6 +11,11 @@ sed -i "s/$NEW/#$NEW/" make.env
 sed -i "s/$OLD/#$OLD/" makesfe.env
 sed -i "s/SFE_FLAG=.*/SFE_FLAG=1/" makesfe.env
 sed -i "s/FLOWOFFLOAD_FLAG=.*/FLOWOFFLOAD_FLAG=0/" makesfe.env
+#sync the kernel version
+KV=$(find /opt/kernel/ -name "boot*+o.tar.gz" | awk -F '[-.]' '{print $2"."$3"."$4"-"$5"-"$6}')
+KVS=$(find /opt/kernel/ -name "boot*+.tar.gz" | awk -F '[-.]' '{print $2"."$3"."$4"-"$5"-"$6}')
+sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KV\"/" make.env
+sed -i "s/^KERNEL_VERSION.*/KERNEL_VERSION=\"$KVS\"/" makesfe.env
 
 for F in *.sh ; do cp $F ${F%.sh}_sfe.sh;done
 find ./* -maxdepth 1 -path "*_sfe.sh" | xargs -i sed -i 's/make\.env/makesfe\.env/g' {}
